a3c7eee1f6e1bdb2fba497cc849466dfd0f7caa0
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-11 05:04:37 +0000

    Done.

---
+++ aoa_test.rb
@@ -0,0 +1,32 @@
+# spice_rack = [
+#   ["Posh", "Scary", "Sporty"],
+#   ["Paprika", "Fajita Mix", "Coriander"],
+#   ["Parsley", "Sage", "Rosemary"]
+# ]
+
+
+#   row_index = 0
+#   while row_index < spice_rack.count do
+#     element_index = 0
+#     while element_index < spice_rack[row_index].count do
+#       puts spice_rack[row_index][element_index] + " sauce"
+#       element_index += 1
+#     end
+#     row_index += 1
+#   end
+
+
+tictactoe = [
+  ["zero", "one", "two"],
+  ["three", "four", "5"],
+  ["6", "7", "8"],
+  ]
+  
+  
+
+  tictactoe.length.times do |r|
+    tictactoe[r].length.times do |c|
+      puts tictactoe[r][c] + " timessss"
+    end
+  end
+ 
--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,50 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  food_arr = []
+    x = 0 
+  while x < src.length do 
+    y = 0
+    food_arr << "I love #{src[x][y]} and #{src[x][y + 1]} on my pizza"
+      while y < src[x].length do 
+        y += 1 
+      end 
+    x += 1 
+   end 
+  food_arr
 end
+
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  max_arr = []
+  x = 0 
+  while x < src.length do 
+    y = 0 
+    if src[x][y] > src[x][y + 1]
+        max_arr.push(src[x][y])
+      else
+        max_arr << src[x][y + 1]
+      end
+    while y < src[x].length do 
+      y +=1 
+    end 
+    x += 1 
+  end 
+  max_arr
 end
 
+
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  total = 0 
+    x = 0 
+    while x < src.length do 
+      y = 0 
+      if src[x][y] % 2 == 0 && src[x][y+1] % 2 == 0 
+          total += (src[x][y] + src[x][y+1])
+        end 
+      while y < src[x].length do
+        y+=1 
+      end 
+      x+=1 
+    end 
+  total 
+  
 end
